b2efd8d9780691067cc62d2e79fd5cfa294bc8bf
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-27 21:45:17 +0600

    Creating a basic react app

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>This is a demo paragraph</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
